58f777cec3df937a55ae4f47b0dbe56652b92ea3
Zachary Yonash <yonash402@gmail.com>
2015-12-11 22:51:03 -0500

    First two challenges complete.

---
+++ AdventOfCode_1.py
@@ -0,0 +1,14 @@
+def solve():
+    counter = 0
+    f = open('AOC1_input.txt', 'r')
+    while True:
+        direction = f.read(1)
+        if direction == '(':
+            counter = counter + 1
+        elif direction == ')':
+            counter = counter - 1
+        if not direction:
+            print "Done stepping! He should be on floor " + str(counter)
+            break
+
+solve()
---
+++ AdventOfCode_2.py
@@ -0,0 +1,17 @@
+def solve():
+    counter = 0
+    position = 0
+    f = open('AOC1_input.txt', 'r')
+    while True:
+        direction = f.read(1)
+        if direction == '(':
+            position = position + 1
+            counter = counter + 1
+        elif direction == ')':
+            position = position - 1
+            counter = counter + 1
+        if position == -1:
+            print "Done stepping! The position of the character is " + str(counter)
+            break
+
+solve()
