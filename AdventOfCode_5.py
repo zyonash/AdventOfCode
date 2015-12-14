440b9287e0fcec818c92e91b4d964feacd754bc9
Zachary Yonash <yonash402@gmail.com>
2015-12-13 22:07:55 -0500

    Finished part 1 of challenge 3

---
+++ AdventOfCode_5.py
@@ -0,0 +1,27 @@
+def solve():
+    x_coordinate = 0
+    y_coordinate = 0
+    houses_seen = 0
+    house_map = {}
+    f = open('AOC5_input.txt', 'r')
+    while True:
+        char = f.read(1)
+        if char == '<':
+            x_coordinate -= 1
+        if char == '>':
+            x_coordinate += 1
+        if char == '^':
+            y_coordinate += 1
+        if char == 'v':
+            y_coordinate -=1
+        if not char:
+            break
+        temp_key = str(x_coordinate)+ " and " + str(y_coordinate)
+        if not temp_key in house_map:
+            print "Haven't seen it"
+            house_map[temp_key] = "Seen"
+            houses_seen += 1
+
+    print houses_seen + 1 # + 1 for the starting location
+
+solve()
